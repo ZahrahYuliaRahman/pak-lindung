8f1ffa72b7527aa3708783079cf8a7f05ffa787c
Zahrah Yulia Rahman <zahrasukma12@gmail.com>
2025-06-20 09:13:57 +0700

    Add files via upload

---
+++ desain/component/dataDiagram.php
@@ -0,0 +1,24 @@
+<?php
+    $prodi = [
+        'Teknologi Informasi' => 31,
+        'Akuntansi Sektor Publik' => 26,
+        'Budidaya Ikan' => 22
+    ];
+    $gender = [
+        'Laki-Laki' => 50,
+        'Perempuan' => 19
+    ];
+    $angkatan = [
+        '2023','2024','2025','2026','2027',
+    ];
+    $angkatanTI = [100, 30, 100, 120, 110];
+    $angkatanASP = [70, 55, 70, 100, 95];
+    $angkatanBDI = [50, 95, 75, 90, 45];
+
+    $minat = [
+        'Baca Buku' => 50,
+        'Menari' => 39,
+        'Tidak ada' => 19,
+        'Menyanyi' => 29
+    ];
+?>
